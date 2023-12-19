c6897e3bb43757f5d55f7737d3964f2e4927d26e
Temesgen Ayneka <temesgen.ayneka@gmail.com>
2023-12-19 23:30:23 +0300

    Update DAO

--- app/src/main/java/com/selamta/localdatabaseusingroom/data/Dao.kt
+++ app/src/main/java/com/selamta/localdatabaseusingroom/data/Dao.kt
@@ -2,10 +2,9 @@ package com.selamta.localdatabaseusingroom.data
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
-import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
-import androidx.room.Update
+
 @Dao
 interface ArticleDao {
     @Insert
