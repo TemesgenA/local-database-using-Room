41630ef3a2ffd713fca4b1d9047b567e58c0db51
Temesgen Ayneka <temesgen.ayneka@gmail.com>
2023-12-19 15:12:57 +0300

    update room version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.selamta.localdatabaseusingroom"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.selamta.localdatabaseusingroom"
@@ -50,11 +50,11 @@ android {
 }
 
 dependencies {
-    val room_version = "2.4.3"
+    val room_version = "2.6.1"
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
